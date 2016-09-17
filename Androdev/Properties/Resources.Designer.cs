9e55fd0831fb58661eb2ec1f8210a3c8d481d274
Fahmi Noor Fiqri <fahminlb33@gmail.com>
2016-09-17 13:21:45 +0700

    Removed unused resource

--- Androdev/Properties/Resources.Designer.cs
+++ Androdev/Properties/Resources.Designer.cs
@@ -63,16 +63,6 @@ namespace Androdev.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap checkmark {
-            get {
-                object obj = ResourceManager.GetObject("checkmark", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap loading {
             get {
                 object obj = ResourceManager.GetObject("loading", resourceCulture);
